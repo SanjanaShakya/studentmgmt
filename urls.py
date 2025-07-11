8dcffd533cb16d3add07d0588f6f6ae12e4391c3
SanjanaShakya <sanjanashakya101@gmail.com>
2025-07-11 16:45:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,26 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for studentmgmt project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from student import urls as student_urls
 
 urlpatterns = [
-    path('', views.get_students, name='get_students'),
-    path('add/', views.add_student, name='add_student'),
-    path('update/<int:pk>/', views.update_student, name='update_student'),
-    path('delete/<int:pk>/', views.delete_student, name='delete_student'), 
-    path('class/', views.StudentListCreateAPIView.as_view(), name='student_class_list_create'),
-    path('class/<int:pk>/', views.StudentDetailAPIView.as_view(), name='student_class_detail'),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')), 
+    path('student/', include('student.urls')),
 ]
 
